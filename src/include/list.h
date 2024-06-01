cb0e9aebde8aabc5c9d15d24cd5c6e6542bf12c1
Jagannath Hari <jagannathhari17@gmail.com>
2024-06-02 00:35:06 +0530

    Added correct function prototype

--- src/include/list.h
+++ src/include/list.h
@@ -8,10 +8,10 @@ typedef struct {
     Node *tail;
 }List;
 
+Node *new_node(char *data);
 void print_list(List *list);
 void free_node(Node *node);
 void clear_list(Node *head);
 void add_node(List *list, char *name);
 void remove_node(Node *current , Node *previous,List *list);
 void free_all_nodes(struct Node* head);
-Node *new_node();
